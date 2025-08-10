414c7a869a445468ff22bcdc6eb2bf8a75445e5f
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-10 23:21:47 +0530

    roboshop-shell1

---
+++ catalogue.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run theis script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
+
+dnf module disable nodejs -y &>> LOGFILE
+
+VALIDATE $? "Disable current NodeJS"
+
+dnf module enable nodejs:18 -y &>> LOGFILE
+
+VALIDATE  $? "Enabling NodeJS:18" 
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? " Installing NodeJs:18"
+
+useradd roboshop  &>> $LOGFILE
+
+VALIDATE $? " Creating roboshop user"
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "creating roboshop user"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading catalogue application"
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping catalogue" 
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing depencies" 
+
+cp /home/centos/roboshop-shell1/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
+VALIDATE $? "copying catalogue service file"
+
+systemctl daemon -reload &>> $LOGFILE
+
+VALIDATE $? " catalogue daemon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+
+VALIDATE $? " Enable catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "Starting catalogue"
+
+cp /home/centos/roboshop-shell1/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "copyitng mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? " Installing MongoDB client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js
+
+VALIDATE $? "LOading catalogue data into MongoDB"
+
+
+
+
+
+
