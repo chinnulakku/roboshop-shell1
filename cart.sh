94c03bfae832e2bf06785603f09f74655c5358ae
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-11 17:30:53 +0530

    roshoshop-shell1

--- cart.sh
+++ cart.sh
@@ -69,7 +69,7 @@ npm install &>> $LOGFILE
 VALIDATE $? "Installing dependencies" 
 
 # use absolute, beacuse catalogue.service exists there
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+cp /home/centos/roboshop-shell1/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
 
 
 VALIDATE $? "copying cart service file"
